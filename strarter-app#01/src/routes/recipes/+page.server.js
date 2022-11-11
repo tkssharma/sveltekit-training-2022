0ed2a1f090d5bf23f6f74d15e211d4c00eaa0024
tkssharma <tarun.softengg@gmail.com>
2022-11-11 14:55:32 +0530

    baseline with vite

---
+++ strarter-app#01/src/routes/recipes/+page.server.js
@@ -0,0 +1,12 @@
+export async function load({ params }) {
+
+  const url = `https://api.github.com/users`;
+
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  return {
+    users: data || []
+  };
+}
